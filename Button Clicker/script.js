7a7e291723145772a1110c1d8dd759cc0bc566eb
ASrini8397 <98121213+ASrini8397@users.noreply.github.com>
2022-04-05 16:17:50 -0700

    Add files via upload

---
+++ Button Clicker/script.js
@@ -0,0 +1,10 @@
+var click= document.querySelector(".btn")
+function Clicked(liked){
+    click.innerText="Logout"
+
+}
+ 
+function Def(){
+    var remove = document.querySelector(".Definition")
+    remove.parentNode.removeChild(remove);
+}
